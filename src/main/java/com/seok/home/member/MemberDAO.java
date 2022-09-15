6743e2f053025eb04d54d3ca539e7f3cda0cc4c9
KOOHYO <zpfhzpfh112@naver.com>
2022-09-16 01:26:04 +0900

    서비스 디에이오 메퍼

--- src/main/java/com/seok/home/member/MemberDAO.java
+++ src/main/java/com/seok/home/member/MemberDAO.java
@@ -1,5 +1,18 @@
 package com.seok.home.member;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class MemberDAO {
 
+	@Autowired
+	private SqlSession sqlSession;
+	private final String NAMESPACE="com.seok.home.member.MemberDAO.";
+	
+	public MemberDTO getLogin(MemberDTO memberDTO)throws Exception{
+		return sqlSession.selectOne(NAMESPACE+"getLogin", memberDTO);
+	}
+	
 }
