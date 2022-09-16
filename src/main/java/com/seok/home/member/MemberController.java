4f0ff1362ca00cfc830d6a3689f823f24fc35d20
KOOHYO <zpfhzpfh112@naver.com>
2022-09-16 15:52:37 +0900

    서비스 연결

--- src/main/java/com/seok/home/member/MemberController.java
+++ src/main/java/com/seok/home/member/MemberController.java
@@ -1,5 +1,6 @@
 package com.seok.home.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +9,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/member/*")
 public class MemberController {
 
+	@Autowired
+	private MemberService memberService;
+	
 	//회원가입
 	@GetMapping("join")
 	public void setJoin()throws Exception{
