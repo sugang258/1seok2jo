c790a58308a00c85539cb16b5626032cac57ff43
KOOHYO <zpfhzpfh112@naver.com>
2022-09-19 10:28:01 +0900

    회원가입 완성, 강사신청 페이지 완성

--- src/main/java/com/seok/home/member/MemberController.java
+++ src/main/java/com/seok/home/member/MemberController.java
@@ -3,6 +3,7 @@ package com.seok.home.member;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,10 +13,22 @@ public class MemberController {
 	@Autowired
 	private MemberService memberService;
 	
+	//강사 회원가입
+	@GetMapping("t_join")
+	public void setTjoin()throws Exception{
+		System.out.println("강사 회원가입 접속(GET)");
+	}
+	
 	//회원가입
 	@GetMapping("join")
 	public void setJoin()throws Exception{
-		System.out.println("회원가입 접속");
+		System.out.println("회원가입 접속(GET)");
+	}
+	
+	//회원가입 폼
+	@PostMapping
+	public void setJoin(MemberDTO memberDTO)throws Exception{
+		System.out.println("회원가입 접속(POST)");
 	}
 	
 	//로그인
@@ -23,4 +36,5 @@ public class MemberController {
 	public String getLogin()throws Exception{
 		return "member/login";
 	}
+	
 }
