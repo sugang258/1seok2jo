17e94cdfcb7ea1eda835e49fdb234dd93e918efc
Hongsugyeong <olivia258@naver.com>
2022-09-29 16:41:42 +0900

    수강신청 중복처리 완료

--- src/main/java/com/seok/home/lecture/add/LectureAddController.java
+++ src/main/java/com/seok/home/lecture/add/LectureAddController.java
@@ -37,42 +37,48 @@ public class LectureAddController {
 	@PostMapping("setLectureAdd")
 	@ResponseBody
 	public int setLectureAdd(LectureDTO lectureDTO, LectureAddDTO lectureAddDTO, HttpServletRequest request,HttpSession session) throws Exception{
+		int result = 1;
 		System.out.println("수강완료");
+		
 		lectureDTO = lectureService.getDetail(lectureDTO);
 		MemberDTO mem = (MemberDTO)request.getSession().getAttribute("member");
-		Date now = new Date();
-		Calendar cal = Calendar.getInstance();
-		cal.setTime(now);
-		cal.add(Calendar.MONTH,lectureDTO.getL_date().intValue());
-		System.out.println(cal.MONTH);
-		java.sql.Date date1 = new java.sql.Date(cal.getTimeInMillis());
-		
-//		LocalDate now = LocalDate.now();
-//		int year=now.getYear();
-//		int datOfMonth = now.getDayOfMonth();
-//		Long monthValue = (long) now.getDayOfMonth();
-//		Long mdate = lectureDTO.getL_date()+monthValue;
-		
-		
-		lectureAddDTO.setS_end(date1);
-		//System.out.println(date);
-		
-		int result = lectureAddService.setLectureAdd(lectureAddDTO);
-		lectureAddService.setLectureCount(lectureAddDTO);
-		
-		StatusDTO statusDTO = new StatusDTO();
-		List<LectureVideoDTO> ar = statusService.getVideoList(lectureDTO);
-		lectureAddDTO.setL_num(lectureDTO.getL_num());
-		//로그인 정보 가져오기
-		lectureAddDTO.setId(mem.getId());
-		lectureAddDTO = lectureAddService.getLectureAdd(lectureAddDTO);
-		
-		for(int i=0; i<ar.size();i++) {
-			statusDTO.setS_num(lectureAddDTO.getS_num());
-			statusDTO.setV_num(ar.get(i).getV_num());
-			//System.out.println(ar.get(i).getV_num());
-			//System.out.println(lectureAddDTO.getS_num());
-			statusService.setStatusAdd(statusDTO);
+		List<LectureAddDTO> list = lectureAddService.getLectureAddAll();
+		for(int i=0;i<list.size(); i++) {
+			if(list.get(i).getId().equals(mem.getId()) && list.get(i).getL_num().equals(lectureAddDTO.getL_num())) {
+				result = 0;
+			}else {
+				result = 1;
+			}
+		}
+		if(result == 1) {
+			
+			Date now = new Date();
+			Calendar cal = Calendar.getInstance();
+			cal.setTime(now);
+			cal.add(Calendar.MONTH,lectureDTO.getL_date().intValue());
+			System.out.println(cal.MONTH);
+			java.sql.Date date1 = new java.sql.Date(cal.getTimeInMillis());
+			
+			lectureAddDTO.setS_end(date1);
+			
+			
+			lectureAddService.setLectureAdd(lectureAddDTO);
+			lectureAddService.setLectureCount(lectureAddDTO);
+			
+			StatusDTO statusDTO = new StatusDTO();
+			List<LectureVideoDTO> ar = statusService.getVideoList(lectureDTO);
+			lectureAddDTO.setL_num(lectureDTO.getL_num());
+			//로그인 정보 가져오기
+			lectureAddDTO.setId(mem.getId());
+			lectureAddDTO = lectureAddService.getLectureAdd(lectureAddDTO);
+			
+			for(int i=0; i<ar.size();i++) {
+				statusDTO.setS_num(lectureAddDTO.getS_num());
+				statusDTO.setV_num(ar.get(i).getV_num());
+				//System.out.println(ar.get(i).getV_num());
+				//System.out.println(lectureAddDTO.getS_num());
+				statusService.setStatusAdd(statusDTO);
+			}
 		}
 		//session.setAttribute("sign", lectureAddDTO);
 		return result;
--- src/main/java/com/seok/home/lecture/add/LectureAddService.java
+++ src/main/java/com/seok/home/lecture/add/LectureAddService.java
@@ -1,5 +1,7 @@
 package com.seok.home.lecture.add;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -23,5 +25,9 @@ public class LectureAddService {
 	
 	public int setLectureCount(LectureAddDTO lectureAddDTO) throws Exception{
 		return lectureAddDAO.setLectureCount(lectureAddDTO);
+	}
+	
+	public List<LectureAddDTO> getLectureAddAll() throws Exception{
+		return lectureAddDAO.getLectureAddAll();
 	}
 }
