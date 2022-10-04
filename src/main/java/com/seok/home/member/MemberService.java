c2e75742270532b801c4c219c912fc6e90b765b7
KOOHYO <zpfhzpfh112@naver.com>
2022-10-04 10:58:09 +0900

    파일 수정중

--- src/main/java/com/seok/home/member/MemberService.java
+++ src/main/java/com/seok/home/member/MemberService.java
@@ -72,46 +72,33 @@ public class MemberService {
 		if(result == 1) {
 			System.out.println("파일이왔니? "+file);
 			
-//			for(LectureFileDTO lf : files) {
-//				lf.setL_num(lectureDTO.getL_num());
-//				result = lectureDAO.setAddFile(lf);
-//				
-//				
-//				if(result!=1) {
-//					System.out.println("파일 추가 오류");
-//					break;
-//				}
-//			}
-//		}else {
-//			System.out.println("lecture에러");
-//		}
+			file.setId(memberDTO.getId());
+			result = memberDAO.setAddFile(file);
 			
 			//수정된 정보를 조회해서 memberDTO에 대입
 			//그럼 memberDTO에는 insert가 되어 null이 아니다
 			memberDTO = memberDAO.getProfile(memberDTO);
-	
+			
 			//memberDTO안에 있는 한개의 memberFileDTO을 
 			//memberFileDTO변수에 대입
 			MemberFileDTO memberFileDTO = memberDTO.getMemberFileDTO();
-	
+			
 			//memberFileDTO가 있으면
 			if(memberFileDTO != null) {
 				//memberFileDTO을 삭제
 				result = memberDAO.setDeleteFile(memberFileDTO);
-				}
-		
-//			String[] url =lectureVideoDTO.getV_url().split(",");
-//			String[] context = lectureVideoDTO.getV_context().split(",");
-//			for(int i=0; i<url.length; i++) {
-//				lectureVideoDTO.setL_num(lectureDTO.getL_num());
-//				lectureVideoDTO.setV_url(url[i]);
-//				lectureVideoDTO.setV_context(context[i]);
-//				lectureVideoDTO.setV_seq((long)i);
-//				lectureDAO.setAddVideo(lectureVideoDTO);
-//				
-//			}
+			}
+			memberDTO.setMemberFileDTO(memberFileDTO);
 			
+			if(result!=1) {
+				System.out.println("파일 추가 오류");
+			}
+			
+		}else {
+			System.out.println("lecture에러");
 		}
+			
+		
 		//(삭제후) memberFileDTO없으면(없으니) 폴더에 추가
 //		String path = "resources/upload/member";
 //		
