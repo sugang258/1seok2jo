7301be04b7cb62be8b64e8f611998a6b64e7335d
Hongsugyeong <olivia258@naver.com>
2022-10-06 17:47:47 +0900

    버튼 클릭 했을 때 디자인 변경

--- src/main/webapp/resources/js/lecture_list.js
+++ src/main/webapp/resources/js/lecture_list.js
@@ -76,6 +76,7 @@ btn.forEach(function(btn){
 
     btn.addEventListener("click",function(event){
         let e = event.target;
+
         let c_num = e.getAttributeNode("data-c-num").value;
         console.log(c_num);
         
@@ -100,3 +101,29 @@ btn.forEach(function(btn){
     })
 })
 
+function handleClick(event) {
+    console.log(event.target);
+
+    console.log(event.target.classList);
+
+    if (event.target.classList[1] === "clicked") {
+      event.target.classList.remove("clicked");
+
+    } else {
+      for (var i = 0; i < btn.length; i++) {
+        btn[i].classList.remove("clicked");
+
+      }
+
+      event.target.classList.add("clicked");
+
+    }
+  }
+
+function init() {
+    for (var i = 0; i < btn.length; i++) {
+      btn[i].addEventListener("click", handleClick);
+    }
+  }
+
+  init();
