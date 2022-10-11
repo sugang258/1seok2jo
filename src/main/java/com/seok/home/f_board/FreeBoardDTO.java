28dff2dfd1aae95c78e6d9972b7775be4836fd6d
KimSeolHee <tjfgml64@nate.com>
2022-10-11 09:15:46 +0900

    add

--- src/main/java/com/seok/home/f_board/FreeBoardDTO.java
+++ src/main/java/com/seok/home/f_board/FreeBoardDTO.java
@@ -13,7 +13,14 @@ public class FreeBoardDTO {
 	private String category;
 	private Long like_count;
 	private String bd_name; //admin에서 게시판 종류 구분하는 용도의 변수
+	private String f_name;
 	
+	public String getF_name() {
+		return f_name;
+	}
+	public void setF_name(String f_name) {
+		this.f_name = f_name;
+	}
 	public Long getFb_num() {
 		return fb_num;
 	}
