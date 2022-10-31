caea9fdec14cd7960cb9d20a8a97454864408c1e
yez-try <skeosk424@gmail.com>
2022-10-31 16:58:47 +0900

    메일매니저

--- src/main/java/com/seok/home/util/EmailManager.java
+++ src/main/java/com/seok/home/util/EmailManager.java
@@ -10,22 +10,25 @@ import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class EmailManager {
-
-    public static void main(String[] args) {
-        String recipient = "skeosk424@gmail.com";
-        String code = "abc";
-
-        // 1. 발신자의 메일 계정과 비밀번호 설정
-        final String user = "simyj12@naver.com";
-        final String password = "password";
+	
+	// 1. 발신자의 메일 계정과 비밀번호 설정
+	final String user = "1s_2j@naver.com";
+	final String password = "1seok2jo!";
+	
+	private Session session;
+	
+    public EmailManager() {
 
         // 2. Property에 SMTP 서버 정보 설정
         Properties prop = new Properties();
         prop.put("mail.smtp.host", "smtp.naver.com");
         prop.put("mail.smtp.port", 465);
         prop.put("mail.smtp.auth", "true");
-//        prop.put("defaultEncoding","utf-8");
+        prop.put("defaultEncoding","utf-8");
         prop.put("mail.smtp.ssl.enable", "true");
         prop.put("mail.smtp.ssl.trust", "smtp.naver.com");
         prop.put("mail.smtp.ssl.protocols", "TLSv1.2");
@@ -34,12 +37,15 @@ public class EmailManager {
 
 
         // 3. SMTP 서버정보와 사용자 정보를 기반으로 Session 클래스의 인스턴스 생성
-        Session session = Session.getDefaultInstance(prop, new javax.mail.Authenticator() {
+        session = Session.getDefaultInstance(prop, new javax.mail.Authenticator() {
             protected PasswordAuthentication getPasswordAuthentication() {
                 return new PasswordAuthentication(user, password);
             }
         });
-
+        
+    }
+    
+    public void sendHTML(String recipient, String subject, String contents) {
         // 4. Message 클래스의 객체를 사용하여 수신자와 내용, 제목의 메시지를 작성한다.
         // 5. Transport 클래스를 사용하여 작성한 메세지를 전달한다.
 
@@ -51,13 +57,12 @@ public class EmailManager {
             message.addRecipient(Message.RecipientType.TO, new InternetAddress(recipient));
 
             // Subject
-            message.setSubject("PLAYDDIT verification code");
+            message.setSubject(subject);
 
             // Text
-            message.setText("Welcome to playddit. your code is ["+code+"]");
+            message.setContent(contents ,"text/html");
 
             Transport.send(message);    // send message
-
 
         } catch (AddressException e) {
             e.printStackTrace();
