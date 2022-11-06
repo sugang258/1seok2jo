85bee3aefff708388354a0d8f5977885af1c2d73
simyeji <skeosk424@gmail.com>
2022-11-06 23:15:59 +0900

    쿠키 저장방법 및 정렬 일부 수정

--- src/main/java/com/seok/home/admin/AdminService.java
+++ src/main/java/com/seok/home/admin/AdminService.java
@@ -100,63 +100,14 @@ public class AdminService {
 		String message = "";
 		if(result==1) {
 			message = "답변 등록에 성공하였습니다.";
-			StringBuffer mailMsg = new StringBuffer();
 			
-			//https://unlayer.com/download-template?url=https://assets.unlayer.com/projects/6070/_generated/zip/1667390037363-gTLhhTwSpiKb9v93.zip&utm_source=automation&utm_medium=email&utm_campaign=lead-d0
-			
-			mailMsg.append("<html><body>");
-			mailMsg.append("<link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT\" crossorigin=\"anonymous\">");
-			
-			mailMsg.append("<div class=\"board\">\r\n"
-					+ "                  <!--내가 문의한 부분-->\r\n"
-					+ "                  <div class=\"board_contents\">\r\n"
-					+ "                    <div class=\"row py-2\">\r\n"
-					+ "                      <div class=\"col-10 align-content-center\" style=\"padding: 8px 8px 8px 0px; font-size: 14px; margin-top: 0.5rem\">\r\n"
-					+ "                        <h5 class=\"px-2\"><b>eee</b></h5>\r\n"
-					+ "                      </div>\r\n"
-					+ "                      <div class=\"col-2 align-content-center my-auto\">\r\n"
-					+ "                        \r\n"
-					+ "                          \r\n"
-					+ "                        <div class=\"b1 d-flex\">\r\n"
-					+ "                          <div class=\"mx-auto\">\r\n"
-					+ "                            <b>\r\n"
-					+ "                              답변완료\r\n"
-					+ "                            </b>\r\n"
-					+ "                          </div>\r\n"
-					+ "                        </div>\r\n"
-					+ "                        \r\n"
-					+ "          \r\n"
-					+ "                      </div>\r\n"
-					+ "                    </div>\r\n"
-					+ "                    <hr style=\"margin: 0;\">\r\n"
-					+ "                    <div class=\"row p-3 text-muted flex-row-reverse\">\r\n"
-					+ "                      2022-11-02\r\n"
-					+ "                    </div>\r\n"
-					+ "                    <div class=\"row\">\r\n"
-					+ "                      <p>eeee</p>\r\n"
-					+ "                    </div>\r\n"
-					+ "                  </div>\r\n"
-					+ "          \r\n"
-					+ "                  <!--답변부분-->\r\n"
-					+ "                  <div class=\"cs_answers\">\r\n"
-					+ "                    <div class=\"mb-3\">\r\n"
-					+ "                      <h5><b>답변</b></h5>\r\n"
-					+ "                      \r\n"
-					+ "                        eeee\r\n"
-					+ "                      \r\n"
-					+ "                        \r\n"
-					+ "                    </div>\r\n"
-					+ "                  </div>\r\n"
-					+ "                </div>");
-			
-			mailMsg.append("<script src=\"https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js\" integrity=\"sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8\" crossorigin=\"anonymous\"></script>");
-			mailMsg.append("</html></body>");
 			csBoardDTO = csDAO.getBoardDetail(csBoardDTO);
-			emailManager.sendHTML(csBoardDTO.getCs_email(), "[일석이조]문의에 답변이 완료되었습니다", mailMsg.toString());
+			
+			String mailText = emailManager.getcsAnswerHtml(csBoardDTO.getCs_title(), csBoardDTO.getCs_contents(), csBoardDTO.getCs_answer());
+			emailManager.sendHTML(csBoardDTO.getCs_email(), "[일석이조]문의에 답변이 완료되었습니다", mailText);
 		}else {
 			message = "답변 등록에 실패하였습니다";
 		}
-		System.out.println(message);
 		
 		return message;
 	}
@@ -195,27 +146,27 @@ public class AdminService {
 	
 	public String getAuth(MemberDTO member) throws Exception{
 		//아이디 패스워드 맞는지 확인
-		
 		member = memberDAO.getLogin(member);
-		
-		if(member==null) {
+		String msg = "";
+		if(member==null) { //일석이조 회원이 아닌 경우
 			return "일석이조 회원 인증 실패";
 		}else {
 			//이미 권한이 있는지 확인
 			for(RoleDTO role:member.getRoleDTOs()) {
 				if(role.getRoleNum()==1L) {
-					return "관리자 권한이 있는 계정입니다.";
+					msg = "관리자 권한이 있는 계정입니다.";
+					break;
 				}
 			}
 			//권한추가
 			int result = memberDAO.setAdminRole(member);
 			if(result==1) {
-				return "인증이 완료되었습니다\n관리자 로그인을 진행해주세요";				
+				msg ="인증이 완료되었습니다 관리자 로그인을 진행해주세요";				
 			}else {
-				return "관리자 인증 실패\n 담당자에게 문의해주세요";
+				msg ="관리자 인증 실패 담당자에게 문의해주세요";
 			}
 		}
-		
+		return msg;
 	}
 	
 	//board삭제
--- src/main/webapp/WEB-INF/views/admin/main.jsp
+++ src/main/webapp/WEB-INF/views/admin/main.jsp
@@ -115,6 +115,6 @@
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
         <script src="/resources/temp/js/scripts.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
-        <script src="/resources/temp/assets/demo/chart-bar-demo.js"></script>
+        <script src="/resources/js/adminDash.js"></script>
     </body>
 </html>
